621695d8a880f158a7a3b4670047211aae3d9e5b
Pavan <pavansundar76@gmail.com>
2022-05-29 14:44:20 +0100

    changes

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,36 @@
-import Dexie from 'dexie';
+import productdb from './module.js'
 
-var db = new Dexie("MyDatabase");
-db.version(1).stores({
-    friends: "++id, name, age,"
-    
-});
 
-db.open();
+
+productdb("productdb",{
+     bulkcreate
+
+	products:'++Idno,Fullname,seller,Price'
+
+	});
+
+const userid = document.getElementById("userid");
+const Fullname = document.getElementById("Fullname");
+const seller = document.getElementById("seller");
+const Price = document.getElementById("Price");
+
+
+const btncreate = document.getElementById("btn-Create");
+const btncreate = document.getElementById("btn-Read");
+const btncreate = document.getElementById("btn-Update");
+const btncreate = document.getElementById("btn-Delete All");
+
+btncreate.onclick=()=>{
+	let flag = bulkcreate(db.product,{
+		name: proname,value,
+		seller: seller.value,
+		price: price.value
+
+
+	})
+
+    console.log(flag)
+}
+
+
+
--- JS/module.js
+++ JS/module.js
@@ -1,13 +1,47 @@
 
 import {JS} from './JS':
 
-const productdb = ()=>{
+const productdb = (dbname,table)=>{
 
-var db = new Dexie("MyDatabase");
-db.version(1).stores({
-    friends: "++id, name, age,"
+const db = new Dexie(dbname)
+db.version(1).stores(table);
     
-});
-
 db.open();
+return db;
+
+}
+
+const bulkcreate = (dbtable,data)=>{
+
+	let flag = empty(data);
+	    if (flag){
+	dbtable.bulkAdd([data]) 
+	console.log("data inserted sucessfully...!");   	
+
+   }else{
+     console.log("please provide Data...!")
+
+   }
+
+	return flag
+}
+
+const empty = object =>{
+	let flag = false;
+	for(const value in object){
+		if (object[value]!=""&&object.has own property(value)){
+			flag = true;
+
+
+		}else{
+			flag = false
+		}
+
+	}
+}
+
+export default productdb;
+export{
+
+	bulkcreate
 }
